5b7c557af980b516de480a47d27b3b6202871366
Rianzim24 <santo.rian@escola.pr.gov.br>
2025-07-01 10:35:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual e a capital do Brasil?',
+    'Brasilia'
+)
+
+criaCartao(
+    'Jogos',
+    'O que e roblox?',
+    'Roblox e uma plataforma de jogos'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual pais e o menor do mundo?',
+    'Vaticando'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual e o maior inseto do mundo',
+    'Bicho-pau Phryganistria chinensis Zhao'
+)
